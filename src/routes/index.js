f7e72141b8bc51258750e094abcd6c990d46057c
Pedro Miranda <pedromirandacn99@gmail.com>
2025-03-27 19:30:43 -0300

    rotas de agendamento

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const userRoutes = require("./userRoutes");
 const authRoutes = require("./userSessionRoutes");
+const scheduleRoutes = require("./scheduleRoutes");
 
 const router = express.Router();
 
+router.use("/", scheduleRoutes);
 router.use("/users", userRoutes);
 router.use("/", authRoutes);
 
